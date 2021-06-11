80aa2e6210cc6a8aaf226b5c19acc2e49781817e
yuchiro22 <t.yuyu.alies@gmail.com>
2021-06-11 22:51:36 +0900

    fix: result screen button design

--- lib/result/result_screen.dart
+++ lib/result/result_screen.dart
@@ -9,13 +9,99 @@ class ResultScreen extends StatefulWidget {
 class ResultScreenState extends State<ResultScreen> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(
-          '結果',
-          style: Theme.of(context).textTheme.headline3,
-        )
-      ],
+    return Scaffold(
+      body: Column(
+        children: [
+          Flexible(
+            child: Text(
+              '結果',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Row(
+            children: [
+              Flexible(
+                child: Column(
+                  children: [
+                    Container(
+                      height: 20,
+                      child: Text('アイデア × 1'),
+                    ),
+                    Container(
+                      height: 20,
+                      child: Text('アイデア × 2'),
+                    ),
+                    Container(
+                      height: 20,
+                      child: Text('アイデア × 3'),
+                    ),
+                    Container(
+                      height: 20,
+                      child: Text('アイデア × 4'),
+                    ),
+                    Container(
+                      height: 20,
+                      child: Text('アイデア × 5'),
+                    ),
+                  ],
+                ),
+              ),
+              Flexible(
+                child: Column(
+                  children: [
+                    Container(
+                      height: 20,
+                      child: Text('アイデア × 6'),
+                    ),
+                    Container(
+                      height: 20,
+                      child: Text('アイデア × 7'),
+                    ),
+                    Container(
+                      height: 20,
+                      child: Text('アイデア × 8'),
+                    ),
+                    Container(
+                      height: 20,
+                      child: Text('アイデア × 9'),
+                    ),
+                    Container(
+                      height: 20,
+                      child: Text('アイデア × 10'),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            width: MediaQuery.of(context).size.width,
+            child: ElevatedButton(
+              child: const Text(
+                'もう一度',
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                primary: Color(0xFFF9DC5F),
+                onPrimary: Colors.white,
+              ),
+              onPressed: () {
+                print('');
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
