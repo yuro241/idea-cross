8f22aa8f8a68d14b5141ff3f1869e3bc0b8db794
yuchiro22 <t.yuyu.alies@gmail.com>
2021-06-12 23:22:33 +0900

    apply home screen design

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -10,69 +10,113 @@ class HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(
-        children: [
-          Flexible(
-            child: Column(
-              children: [
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-                  child: Text(
-                    '掛け合わせでアイデア出し',
-                    style: Theme.of(context).textTheme.headline5,
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-                  child: Text(
-                    'コンセプトを入力すると、それに合った単語を掛け合わせます。アイデア出しをもっと楽しく、おもしろく！',
-                    style: Theme.of(context).textTheme.bodyText1,
-                  ),
-                ),
-              ],
+      appBar: AppBar(
+        backgroundColor: Color(0xFFFFFFFF),
+        title: Container(
+          child: Text(
+            'IDEA × CROSS',
+            style: TextStyle(
+              color: Color(0xFF112D49),
             ),
           ),
-          Flexible(
-            child: Column(
-              children: [
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 8,
-                    vertical: 16,
-                  ),
-                  child: Text(
-                    'アイデアを入力',
-                    style: Theme.of(context).textTheme.headline5,
+        ),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Text(
+              '掛け合わせでアイデア出し',
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF11254A),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Text(
+              'コンセプトを入力すると、それに合った単語を掛け合わせます。アイデア出しをもっと楽しく、おもしろく！',
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.normal,
+                color: Color(0xFF11254A),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 56,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Text(
+              'アイデアを入力',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF11254A),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 12,
+          ),
+          Container(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'アイデアを入力',
+                ),
+              ),
+            ),
+            height: 42,
+          ),
+          SizedBox(
+            height: 24,
+          ),
+          Container(
+            height: 40,
+            width: MediaQuery.of(context).size.width,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/result');
+                },
+                child: Text(
+                  'IDEA CROSS!',
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: 'アイデアを入力',
-                    ),
-                  ),
+                style: ElevatedButton.styleFrom(
+                  primary: Color(0xFFF9DC5F),
+                  onPrimary: Colors.white,
                 ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-                  child: TextButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/result');
-                    },
-                    child: Text('Idea Cross!'),
-                  ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            child: TextButton(
+              onPressed: () {
+                print('on pressed 単語一覧を見る button');
+              },
+              child: Text(
+                '単語一覧を見る',
+                style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.normal,
+                  color: Color(0xFF747474),
                 ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-                  child: TextButton(
-                    onPressed: () {
-                      print('on pressed 単語一覧を見る button');
-                    },
-                    child: Text('単語一覧を見る'),
-                  ),
-                ),
-              ],
+              ),
             ),
           ),
         ],
