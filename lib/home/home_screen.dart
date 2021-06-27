60532da897c39ebb624963566bded79fc987e3a1
yuchiro22 <t.yuyu.alies@gmail.com>
2021-06-27 13:58:24 +0900

    add background image

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -31,71 +31,79 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          SizedBox(
-            height: 246,
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/background.png'),
           ),
-          Row(
-            children: [
-              AboutApp(),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 20),
-                    child: Text(
-                      'アイデアを入力',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xFF11254A),
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 12,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 20),
-                    child: SearchTextField(controller: textFieldController),
-                  ),
-                  SizedBox(
-                    height: 24,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 20),
-                    child: IdeaCrossButton(onPressed: () {
-                      Navigator.pushNamed(
-                        context,
-                        '/result',
-                        arguments:
-                            ResultScreenArguments(textFieldController.text),
-                      );
-                    }),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-                    child: TextButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, '/wordList');
-                      },
+        ),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 246,
+            ),
+            Row(
+              children: [
+                AboutApp(),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 20),
                       child: Text(
-                        '単語一覧を見る',
+                        'アイデアを入力',
                         style: TextStyle(
-                          fontSize: 12,
-                          fontWeight: FontWeight.normal,
-                          color: Color(0xFF747474),
-                          decoration: TextDecoration.underline,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF11254A),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ],
+                    SizedBox(
+                      height: 12,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 20),
+                      child: SearchTextField(controller: textFieldController),
+                    ),
+                    SizedBox(
+                      height: 24,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 20),
+                      child: IdeaCrossButton(onPressed: () {
+                        Navigator.pushNamed(
+                          context,
+                          '/result',
+                          arguments:
+                              ResultScreenArguments(textFieldController.text),
+                        );
+                      }),
+                    ),
+                    Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                      child: TextButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/wordList');
+                        },
+                        child: Text(
+                          '単語一覧を見る',
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.normal,
+                            color: Color(0xFF747474),
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
